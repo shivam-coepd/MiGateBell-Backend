b42054deffd7c8e41de24164d6f35c81bcdb5cfd
shivam-coepd <shivam.coepd@gmail.com>
2026-06-08 11:28:34 +0530

    Token auto refresh related changes

--- app/helpers/jwt_helper.php
+++ app/helpers/jwt_helper.php
@@ -67,3 +67,20 @@ function jwt_validate_token($token) {
     return false;
   }
 }
+
+function get_authorization_header() {
+  $headers = null;
+  if (isset($_SERVER['Authorization'])) {
+    $headers = trim($_SERVER["Authorization"]);
+  } else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { // Nginx or fast CGI
+    $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+  } elseif (function_exists('apache_request_headers')) {
+    $requestHeaders = apache_request_headers();
+    // Server-side fix for header name casing
+    $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+    if (isset($requestHeaders['Authorization'])) {
+      $headers = trim($requestHeaders['Authorization']);
+    }
+  }
+  return $headers;
+}
